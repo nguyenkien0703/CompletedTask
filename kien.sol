44adbfcc3d3faae0c77c623c823db138fe56c571
Nguyễn Văn KIên <94183591+nguyenkien0703@users.noreply.github.com>
2023-06-14 16:27:15 +0700

    Update kien.sol

--- kien.sol
+++ kien.sol
@@ -14,45 +14,22 @@ contract Kien is ERC20, AccessControl{
         _setupRole(BURNER_ROLE, msg.sender);
     }
     
-    // view role'bytes32
-    function getMinterRole() public view  returns (bytes32) {
-       // return keccak256("MINTER_ROLE");
-       return MINTER_ROLE;
-    }
-
-    function getBurnerRole() public view  returns (bytes32) {
-        // return keccak256("MINTER_ROLE");
-        return BURNER_ROLE; 
-    }
-
+   
 
 
     // do commission 
-    function mint(address to, uint256 amount ) public {
+    function mint(address to, uint256 amount ) external {
         require(hasRole(MINTER_ROLE,msg.sender), "You don't have mint right");
         _mint(to,amount);
 
     }
-    function burn(address from,uint256 amount) public {
+    function burn(address from,uint256 amount) external {
         require(hasRole(BURNER_ROLE,msg.sender),"You don't have burn rights");
         _burn(from, amount);
     }
 
 
-    // granted for a account 
-    function grantedMinterRole(address account ) public {
-        grantRole(MINTER_ROLE, account);
-    }
-    function grantedBurnRole(address account) public {
-        grantRole(BURNER_ROLE, account);
-    }
-    // check account'role 
-    function hasMinterRole(address account ) public  view returns (bool) {
-        return  hasRole(MINTER_ROLE, account);
-    }
-    function hasBurnRole(address account ) public view returns (bool ){
-        return hasRole(BURNER_ROLE, account);
-    }
+    
 
 }
 
